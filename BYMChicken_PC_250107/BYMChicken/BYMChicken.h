a15d49682c13dc2866126aae47ad7de0435e21de
BYMParkJinPyo <wlsvy04@bymtech.com>
2025-01-08 10:08:19 +0900

    250108 1007 v1.0 first file test

---
+++ BYMChicken_PC_250107/BYMChicken/BYMChicken.h
@@ -0,0 +1,32 @@
+﻿
+// BYMChicken.h: BYMChicken 응용 프로그램의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CBYMChickenApp:
+// 이 클래스의 구현에 대해서는 BYMChicken.cpp을(를) 참조하세요.
+//
+
+class CBYMChickenApp : public CWinAppEx
+{
+public:
+	CBYMChickenApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+	
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBYMChickenApp theApp;
